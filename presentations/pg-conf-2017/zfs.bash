3fc63932c147e03eba460d2d765426868a244b5e
Akshay Gupta <kitallis@gmail.com>
2017-03-02 15:29:23 +0530

    adding slides for pgconf2017

---
+++ presentations/pg-conf-2017/zfs.bash
@@ -0,0 +1,5 @@
+| id | type    | upstream_node_id | cluster | name  | conninfo  | slot_name | priority | active |
+|----+---------+------------------+---------+-------+-----------+-----------+----------+--------|
+|  1 | master  |                  | test    | node1 | localhost | rs1       |      100 | t      |
+|  2 | standby |                1 | test    | node2 | locahost  | rs2       |       99 | t      |
+|  3 | standby |                1 | test    | node3 | localhost | rs3       |       98 | t      |
