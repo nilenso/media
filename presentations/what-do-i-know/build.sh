755134c828b18e8f0fb735f2e45f1f3db3eef02f
Steven Deobald <steven@nilenso.com>
2018-07-04 18:03:16 +0530

    what do i know slides

---
+++ presentations/what-do-i-know/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# pdflatex was used originally but cannot render Sanskrit without a lot of hoop-jumping
+# pdflatex vipassana-for-hackers.tex
+
+# xelatex is apparently less capable than pdflatex at font-rendering in general, but
+# is capable of rendering non-Latin fonts (like Hindi/Sanskrit) by default
+
+# Compile twice to resolve references.
+
+xelatex what-do-i-know.tex
+xelatex what-do-i-know.tex
+
